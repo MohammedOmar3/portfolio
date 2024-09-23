ad233dd44bfd37cfe2787a8d1d5c1d094bdc92da
Mohammed Omar <o.mohammed3@hotmail.com>
2024-09-23 12:04:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import Services from './Components/Services'
+import Works from './Components/Works'
+import Resume from './Components/Resume'
+import Skills from './Components/Skills'
+import Testimonials from './Components/Testimonials'
+import Contact from './Components/Contact'
+import Footer from './Components/Footer'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Works></Works>
+      <Services></Services>
+      <Resume></Resume>
+      <Skills></Skills>
+      <Testimonials></Testimonials>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Navbar.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { motion } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-cions';
+
+const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen( !isMenuOpen );
+    }
+
+  return (
+    <div className='navbar'>
+        <div className="logo">
+            <img src="/logo.png" alt="logo" className="App-logo" />
+            <a href="mailto:example.com?subject=Contact&body=Hello!">info@codewithmxmd.com</a>
+
+            <div className='menu'>
+                <button className="hamburger" onClick={toggleMenu}>
+                    <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} size="lg" />
+                </button>
+
+                <ul className={`nav-list ${isMenuOpen ? 'show' : ''}`}>
+                    <li className="nav-item">
+                        <a href='#services' className='nav-link'>Services</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href='#works' className='nav-link'>Works</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href='#resume' className='nav-link'>Resume</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href='#skills' className='nav-link'>Skills</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href='#testimonials' className='nav-link'>Testimonials</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href='#contact' className='nav-link'>Contact</a>
+                    </li>
+
+                    <li className="nav-item">
+                        <motion.a 
+                            href="/resume.pdf" 
+                            download="Mohammed_Omar_CV.pdf"
+                            className="resume-button"
+                            initial={{ scale: 1 }}
+                            whileHover={{ scale: 1.05}}
+                            transition={{ duration: 0.3 }}
+                        >
+                            Resume
+                            <img src="download.png" alt="Download icon" className="resume-icon" />
+                        </motion.a>
+                    </li>
+
+                </ul>
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
